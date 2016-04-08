e57db4c2397777bd18c13ecbbc4188aadada3077
colophonemes <dev@colophonemes.com>
2016-04-08 15:10:14 +0100

    Delete/recreate styles directory on script rebuild

--- tasks/styles.js
+++ tasks/styles.js
@@ -3,6 +3,7 @@ console.log('Building Stylesheets')
 var fs = require('fs')
 var path = require('path')
 var cp = require('ncp')
+var rm = require('rimraf')
 
 var sass = require('node-sass')
 var cleanCSS = require('clean-css')
@@ -13,48 +14,54 @@ var srcFile = path.join(__dirname,'..','src','styles','styles.scss');
 var destPath = path.join(__dirname,'..','src','metalsmith','styles')
 var builtPath = path.join(__dirname,'..','dest')
 
-sass.render({
-  file: srcFile,
-  outFile: 'app.css',
-  sourceMap: 'app.css.map'
+rm(destPath,function(err){
+	if(err) throw err;
+	fs.mkdir(destPath, function(err){
+		if(err) throw err;
+		sass.render({
+		  file: srcFile,
+		  outFile: 'app.css',
+		  sourceMap: 'app.css.map'
 
-}, function(err, result) {
-	if(err) throw err
-	console.log('Processed SCSS files')
-	var files = {
-		"app.css" : result.css,
-		"app.css.map" : result.map
-	}
+		}, function(err, result) {
+			if(err) throw err
+			console.log('Processed SCSS files')
+			var files = {
+				"app.css" : result.css,
+				"app.css.map" : result.map
+			}
 
-	cleaned = new cleanCSS({
-		sourceMap: result.map.toString(),
-		debug:true
-	}).minify(result.css);
-	files["app.min.css"] = cleaned.styles.toString() + '\n/*# sourceMappingURL=app.min.css.map */';
-	files["app.min.css.map"] = cleaned.sourceMap;
-	console.log('Minified CSS file')
-	console.log('Original vs Minified:',(cleaned.stats.originalSize/1000) + 'kb','/',(cleaned.stats.minifiedSize/1000) + 'kb','('+(cleaned.stats.efficiency*100).toFixed(2)+'% reduction)')
-	
+			cleaned = new cleanCSS({
+				sourceMap: result.map.toString(),
+				debug:true
+			}).minify(result.css);
+			files["app.min.css"] = cleaned.styles.toString() + '\n/*# sourceMappingURL=app.min.css.map */';
+			files["app.min.css.map"] = cleaned.sourceMap;
+			console.log('Minified CSS file')
+			console.log('Original vs Minified:',(cleaned.stats.originalSize/1000) + 'kb','/',(cleaned.stats.minifiedSize/1000) + 'kb','('+(cleaned.stats.efficiency*100).toFixed(2)+'% reduction)')
+			
 
-	each(Object.keys(files),function(file,cb){
-		fs.writeFile(path.join(destPath,file),files[file],function(err){
-			console.log('+ Wrote file',file)
-			cb(err);
-		})
-	},
-	function(err){
-		if(err) console.log(err)
-		console.log('All files written!')
-		if(process.env.ENV !== 'production'){
-			fs.readdir(builtPath,function(err){
-				if(err) {
-					return;
-				}
-				cp(destPath,path.join(builtPath,'styles'),function(){
-					console.log('Updated /dest directory with latest styles...')
+			each(Object.keys(files),function(file,cb){
+				fs.writeFile(path.join(destPath,file),files[file],function(err){
+					console.log('+ Wrote file',file)
+					cb(err);
 				})
-			})
-		}
+			},
+			function(err){
+				if(err) console.log(err)
+				console.log('All files written!')
+				if(process.env.ENV !== 'production'){
+					fs.readdir(builtPath,function(err){
+						if(err) {
+							return;
+						}
+						cp(destPath,path.join(builtPath,'styles'),function(){
+							console.log('Updated /dest directory with latest styles...')
+						})
+					})
+				}
 
-	})
-});
+			})
+		});
+	});
+})
