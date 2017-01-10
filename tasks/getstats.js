8fc5c6449e00b5d7b8832fbff14ac36950c91397
Sam Deere <dev@colophonemes.com>
2017-01-10 13:07:13 -0800

    Fix hacky stats calcs now DB is updated

--- tasks/getstats.js
+++ tasks/getstats.js
@@ -7,8 +7,6 @@ var destPath = path.join(__dirname,'..','src','metalsmith','settings')
 
 console.log('Getting Stats')
 getStats.get(function(stats){
-  stats.amountPledged = stats.amountPledged + (250 * 1000 * 1000)
-  stats.amountPledgedWords = '$1.3 billion'
 	fs.writeFile(path.join(destPath,'stats.json'),JSON.stringify(stats),function(err){
 		if (err) throw err;
 		console.log('Stats saved to settings/stats.json')
